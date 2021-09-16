c87533d98f2c63bad367adef459666449397c785
Tzheng456 <Tzheng456@gmail.com>
2021-09-15 17:16:29 -0700

    implement exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@new_store = Store.new
+
+puts "enter store name:"
+
+@new_store.name = gets.chomp
+@new_store.save
+@new_store.errors.messages.each do |error_msg|
+  puts "Err: #{error_msg}"
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than: 0 }
+  validate :mens_or_womens_apparel_must_include
+
+  def mens_or_womens_apparel_must_include
+    errors[:base] << "Must specify if store sells either men or women's apparel" unless mens_apparel || womens_apparel
+  end
 end
